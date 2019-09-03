b01e6e2bf052d4ebd2e9250f4abfddd9a09f5c9d
SegFault42 <SegFault42@protonmail.com>
2019-09-03 16:40:38 +0200

    get all links

--- contentHtml/htmlContent.go
+++ contentHtml/htmlContent.go
@@ -2,28 +2,57 @@ package contentHtml
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+
+	"github.com/anaskhan96/soup"
 )
 
 func GetHtmlContent(link string) {
-	req, err := http.NewRequest("GET", link, nil)
+
+	// HTTP get request
+	//resp, err := http.Get(link)
+	//if err != nil {
+	//panic(err)
+	//}
+	//defer resp.Body.Close()
+
+	//// Convert response as string
+	//dataInBytes, err := ioutil.ReadAll(resp.Body)
+	//if err != nil {
+	//panic(err)
+	//}
+	//pageContent := string(dataInBytes)
+
+	//titleStartIndex := strings.Index(pageContent, `<div id="all" style=" display: none; ">`)
+	//if titleStartIndex == -1 {
+	//fmt.Println("No title element found")
+	//os.Exit(0)
+	//}
+
+	//fmt.Printf("%v\n", titleStartIndex)
+	//titleStartIndex += 39
+
+	//titleEndIndex := strings.Index(pageContent, `<div id="ppp" style="">`)
+	//if titleEndIndex == -1 {
+	//fmt.Println("No closing tag for title found.")
+	//os.Exit(0)
+	//}
+	//fmt.Printf("%v\n", titleEndIndex)
+
+	//pageTitle := []byte(pageContent[titleStartIndex:titleEndIndex])
+
+	//// Print out the result
+	//fmt.Printf("Page title: %s\n", pageTitle)
+	resp, err := soup.Get(link)
 	if err != nil {
 		panic(err)
 	}
 
-	// Do request
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+	doc := soup.HTMLParse(resp)
 
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
+	fmt.Println(doc.Text())
+	// <div id="all" style=" display: none; ">
+	links := doc.Find("div", "id", "all").FindAll("img")
+	for _, link := range links {
+		fmt.Println(link.Attrs()["data-src"])
 	}
-
-	fmt.Println(string(content))
-	fmt.Printf("status = %d\n", resp.StatusCode)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go-japscan-scrapper
 
 go 1.12
 
-require github.com/akamensky/argparse v0.0.0-20190829110830-5293d9863374
+require (
+	github.com/akamensky/argparse v0.0.0-20190829110830-5293d9863374
+	github.com/anaskhan96/soup v1.1.1
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
+)
